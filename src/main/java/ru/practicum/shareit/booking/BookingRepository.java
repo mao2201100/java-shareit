c9c65eea4f8993ccdc305b690d005e7f3246d260
Mao2201 <tsybin.a.a@lan-profi.ru>
2023-04-21 22:16:49 +0300

    Правки ошибок тестов попытка 2

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -11,7 +11,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 //    // поиск всех записей по переданному bookerId с датой окончания (поле end ) раньше переданной.
 //    List<Booking> findByBookerId(Long bookerId);
 
-    @Query(value = "select * from booking book inner join item i on i.id = book.item_id where item_id = :itemId and i.owner_id = :ownerId and book.status = 'APPROVED' and cast(book.end_date as date) = cast(now() as date) order by book.end_date desc limit 1", nativeQuery = true)
+    @Query(value = "select * from booking book inner join item i on i.id = book.item_id where item_id = :itemId and i.owner_id = :ownerId and book.status = 'APPROVED' order by book.end_date desc limit 1", nativeQuery = true)
     Booking fetchLastBookerByItem(long itemId, long ownerId);
 
     @Query(value = "select * from booking book where item_id = :itemId and book.id not in (select bookSub1.id from booking bookSub1  inner join item iSub1 on iSub1.id = bookSub1.item_id where bookSub1.status = 'APPROVED' and bookSub1.item_id = :itemId and iSub1.owner_id = :ownerId and bookSub1.status = 'APPROVED' order by bookSub1.start_date desc limit 1) and exists((select bookSub1.id from booking bookSub1  inner join item iSub1 on iSub1.id = bookSub1.item_id where bookSub1.item_id = :itemId and iSub1.owner_id = :ownerId and bookSub1.status = 'APPROVED' order by bookSub1.start_date desc limit 1)) and book.booker_id not in (select bookSub2.booker_id from booking bookSub2  inner join item isub2 on isub2.id = bookSub2.item_id where bookSub2.item_id = :itemId and isub2.owner_id = :ownerId  and bookSub2.status = 'APPROVED' order by bookSub2.start_date desc limit 1) order by book.start_date desc limit 1", nativeQuery = true)
