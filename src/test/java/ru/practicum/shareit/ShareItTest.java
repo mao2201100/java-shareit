5145ecd7e2a6dc5e6d3f1fb3612e19d1f9255f4d
Mao2201 <tsybin.a.a@lan-profi.ru>
2023-04-21 11:07:13 +0300

    Исправил код стайл 8 попытка

---
+++ src/test/java/ru/practicum/shareit/ShareItTest.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class ShareItTest {
+
+    @SpringBootTest
+    public class ContextLoadsTest {
+
+        @Test
+        void contextLoads() {
+
+        }
+    }
+}
+
+
+
+
